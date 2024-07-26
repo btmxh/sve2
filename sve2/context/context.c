04cc99f668e15b65fa90c9101afb5c62b13c080b
btmxh <ngoduyanh.chip@gmail.com>
2024-07-26 18:15:55 +0700

    docs: :memo: document context api

--- sve2/context/context.c
+++ sve2/context/context.c
@@ -148,6 +148,7 @@ static ma_format get_ma_sample_format(enum AVSampleFormat format) {
 }
 
 context_t *context_init(const context_init_t *info) {
+  // initialize core libraries
   init_logging();
   init_threads_timer();
 
@@ -156,6 +157,7 @@ context_t *context_init(const context_init_t *info) {
   c->xscale = 1.0;
   c->yscale = 1.0;
 
+  // GLFW initialization
   nassert(
       !glfwSetErrorCallback(glfw_error_callback) &&
       "Existing GLFW error callback is overriden. Consider setting the GLFW "
@@ -168,32 +170,41 @@ context_t *context_init(const context_init_t *info) {
   // we require EGL + OpenGL ES for VAAPI integration extensions
   glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API);
   glfwWindowHint(GLFW_CONTEXT_DEBUG, GLFW_TRUE);
-  // nassert((c->window = glfwCreateWindow(info->width, info->height,
-  //                                       "sve2 window", NULL, NULL)));
-  c->window =
-      glfwCreateWindow(info->width, info->height, "sve2 window", NULL, NULL);
+  nassert((c->window = glfwCreateWindow(info->width, info->height,
+                                        "sve2 window", NULL, NULL)));
   glfwMakeContextCurrent(c->window);
   glfwSetWindowUserPointer(c->window, c);
+
+  // GLAD function loading
   nassert(gladLoadGL(glfwGetProcAddress));
   nassert(gladLoadEGL(glfwGetEGLDisplay(), glfwGetProcAddress));
 
+  // enable debug output
+  // comment these lines (and set GLFW_CONTEXT_DEBUG to GLFW_FALSE above to
+  // disable this)
   glEnable(GL_DEBUG_OUTPUT);
   glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
   glDebugMessageCallback(gl_debug_callback, NULL);
 
+  // get framebuffer size
+  // the window size might be different to the size specified in
+  // glfwCreateWindow (e.g. if one is using a tiling WM (me))
   int width, height;
   glfwGetFramebufferSize(c->window, &width, &height);
   c->info.width = width;
   c->info.height = height;
 
+  // GLFW callback
   glfwSetFramebufferSizeCallback(c->window, glfw_framebuffer_callback);
   glfwSetWindowContentScaleCallback(c->window, glfw_content_scale_callback);
 
   shader_manager_init(&c->sman, "shaders/out");
 
+  // mode-specific initialization
   if (c->info.mode == CONTEXT_MODE_RENDER) {
     nassert(c->rctx.hw_frame = av_frame_alloc());
     nassert(c->rctx.transfer_frame = av_frame_alloc());
+    // TODO: this only supports RGB -> NV12 conversion
     nassert(c->rctx.color_convert_shader =
                 shader_new_c(c, "encode_nv12.comp.glsl"));
     muxer_init(&c->rctx.muxer, c->info.output_path);
@@ -201,12 +212,14 @@ context_t *context_init(const context_init_t *info) {
     const AVCodec *video_codec, *audio_codec;
     nassert(video_codec = avcodec_find_encoder_by_name("hevc_vaapi"));
     nassert(audio_codec = avcodec_find_encoder(AV_CODEC_ID_PCM_S16LE));
+    // create streams for output file
     c->rctx.video_si =
         muxer_new_stream(&c->rctx.muxer, c, video_codec, true, NULL, NULL);
     c->rctx.audio_si =
         muxer_new_stream(&c->rctx.muxer, c, audio_codec, false, NULL, NULL);
     muxer_begin(&c->rctx.muxer);
 
+    // create OpenGL capturing objects
     i32 width = c->info.width, height = c->info.height;
     glCreateFramebuffers(1, &c->rctx.fbo);
     glCreateTextures(GL_TEXTURE_2D, 1, &c->rctx.fbo_color_attachment);
@@ -222,16 +235,21 @@ context_t *context_init(const context_init_t *info) {
             GL_FRAMEBUFFER_COMPLETE);
     c->rctx.uv_offset_y = height;
     hw_align_size(NULL, &c->rctx.uv_offset_y);
+    // TODO: NV12-specific hack
     i32 nv12_width = width, nv12_height = c->rctx.uv_offset_y + height / 2;
     glCreateTextures(GL_TEXTURE_2D, 1, &c->rctx.output_texture);
     glTextureStorage2D(c->rctx.output_texture, 1, GL_R8, nv12_width,
                        nv12_height);
   } else {
+    // allocate audio playback buffer and other things
     i32 num_samples =
         c->info.sample_rate * c->info.num_buffered_audio_frames / c->info.fps;
     nassert(
         c->pctx.audio_fifo = av_audio_fifo_alloc(
             c->info.sample_fmt, c->info.ch_layout->nb_channels, num_samples));
+    // throughout this, we assumed that audio samples are interleaved (to
+    // advance the pointer), therefore we require the sample format to be
+    // non-planar
     c->pctx.audio_staging_buffer =
         sve2_malloc(num_samples * c->info.ch_layout->nb_channels *
                     av_get_bytes_per_sample(c->info.sample_fmt));
@@ -248,6 +266,7 @@ context_t *context_init(const context_init_t *info) {
     config.sampleRate = c->info.sample_rate;
     config.dataCallback = ma_data_callback;
     config.pUserData = c;
+    // the period size is 1 frame
     config.periodSizeInFrames = c->info.sample_rate / c->info.fps;
     nassert(ma_device_init(NULL, &config, &c->pctx.audio_device) == MA_SUCCESS);
     nassert(ma_device_start(&c->pctx.audio_device) == MA_SUCCESS);
@@ -280,21 +299,13 @@ void context_free(context_t *c) {
   done_logging();
 }
 
+// this is baked-in to GLFW, so nice
 void context_set_should_close(context_t *c, bool should_close) {
   glfwSetWindowShouldClose(c->window, should_close);
 }
 
 bool context_get_should_close(context_t *c) {
   return glfwWindowShouldClose(c->window);
-}
-
-i64 context_get_time(context_t *c) {
-  switch (c->info.mode) {
-  case CONTEXT_MODE_RENDER:
-    return (i64)c->frame_num * SVE2_NS_PER_SEC / c->info.fps;
-  default:
-    return threads_timer_now();
-  }
 }
 
 void context_get_framebuffer_info(context_t *c, i32 *w, i32 *h, f32 *xscale,
@@ -314,16 +325,18 @@ void context_begin_frame(context_t *c) {
 
   if (c->info.mode == CONTEXT_MODE_RENDER) {
     glBindFramebuffer(GL_FRAMEBUFFER, c->rctx.fbo);
-    c->num_frame_samples = 0;
   }
 
   i32 width, height;
   context_get_framebuffer_info(c, &width, &height, NULL, NULL);
   glViewport(0, 0, width, height);
+
+  c->num_frame_samples = 0;
 }
 
 void context_end_frame(context_t *c) {
   if (c->info.mode == CONTEXT_MODE_RENDER) {
+    // do color-conversion via compute shader
     i32 width = c->info.width, height = c->info.height;
     nassert(shader_use(c->rctx.color_convert_shader) >= 0);
     glUniform1i(0, c->rctx.uv_offset_y);
@@ -335,6 +348,7 @@ void context_end_frame(context_t *c) {
     glDispatchCompute(width / 2, height / 2, 1);
     glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
 
+    // submit frame to hardware-accelerated video encoder
     c->rctx.hw_frame->hw_frames_ctx = av_buffer_ref(
         c->rctx.muxer.encoders[c->rctx.video_si].c->hw_frames_ctx);
     c->rctx.hw_frame->width = width;
@@ -359,18 +373,33 @@ GLuint context_default_framebuffer(context_t *c) {
 }
 
 void context_set_audio_timer(context_t *c, i64 time) {
+  // this should make sense from seeing the implementation of
+  // context_get_audio_timer()
   c->num_samples_from_last_seek = 0;
   c->audio_timer_offset = time;
 }
 
 i64 context_get_audio_timer(context_t *c) {
   i32 audio_fifo_size = 0;
+  // in preview mode, there is a little difference:
+  // samples submitted to the context might not have been played
+  // the number of such samples can be found by getting the size of the audio
+  // fifo buffer
   if (c->info.mode == CONTEXT_MODE_PREVIEW) {
     sve2_mtx_lock(&c->pctx.audio_fifo_mutex);
     audio_fifo_size = av_audio_fifo_size(c->pctx.audio_fifo);
     sve2_mtx_unlock(&c->pctx.audio_fifo_mutex);
   }
+  //                             _______________ < this is `audio_fifo_size`
+  //                             samples
+  //      we need to count this v        |
+  // ____________________________        v
+  // XXXXXXXXXXXXXXXXXXXXXXXXXXXXOOOOOOOOOOOOOOO: c->num_samples_from_last_seek
+  // samples in total
+  //                            | --- samples submitted that has not been played
   i32 time = c->num_samples_from_last_seek - audio_fifo_size;
+  // we add with the seek offset, and convert `time` (in samples unit) to
+  // nanoseconds
   return c->audio_timer_offset + time * SVE2_NS_PER_SEC / c->info.sample_rate;
 }
 
@@ -378,14 +407,19 @@ bool context_map_audio(context_t *c, void *staging_buffer[static 1],
                        i32 nb_samples[static 1]) {
   switch (c->info.mode) {
   case CONTEXT_MODE_PREVIEW:
+    // in preview mode, we use the staging buffer directly
+    // the number of samples is the remaining space in the audio fifo to not let
+    // the fifo get too big
     sve2_mtx_lock(&c->pctx.audio_fifo_mutex);
     *nb_samples = av_audio_fifo_space(c->pctx.audio_fifo);
     *staging_buffer = c->pctx.audio_staging_buffer;
     sve2_mtx_unlock(&c->pctx.audio_fifo_mutex);
     break;
   case CONTEXT_MODE_RENDER:
+    // in render mode, we use the buffer from transfer_frame
     AVFrame *frame = c->rctx.transfer_frame;
     av_frame_unref(frame);
+    // prepare frame params for allocation
     av_channel_layout_copy(&frame->ch_layout, c->info.ch_layout);
     frame->sample_rate = c->info.sample_rate;
     frame->nb_samples = frame->sample_rate / c->info.fps - c->num_frame_samples;
@@ -393,18 +427,23 @@ bool context_map_audio(context_t *c, void *staging_buffer[static 1],
     if (frame->nb_samples <= 0) {
       return false;
     }
+    // sample allocation for the frame
+    // we must allocate every frame because the muxer would take control of this
+    // memory, so we can't allocate once like in preview mode
     nassert_ffmpeg(av_frame_get_buffer(frame, 0));
     *nb_samples = frame->nb_samples;
     *staging_buffer = frame->data[0];
     break;
   }
 
+  // we only map samples if the number of writable samples is positive
   return *nb_samples > 0;
 }
 
 void context_unmap_audio(context_t *c, i32 nb_samples) {
   switch (c->info.mode) {
   case CONTEXT_MODE_PREVIEW:
+    // write from staging buffer to audio fifo
     sve2_mtx_lock(&c->pctx.audio_fifo_mutex);
     av_audio_fifo_write(c->pctx.audio_fifo,
                         (void *[]){c->pctx.audio_staging_buffer}, nb_samples);
@@ -414,11 +453,13 @@ void context_unmap_audio(context_t *c, i32 nb_samples) {
     AVFrame *frame = c->rctx.transfer_frame;
     frame->nb_samples = nb_samples;
     frame->pts = c->num_total_samples;
+    // submit allocated frame to muxer directly
     muxer_submit_frame(&c->rctx.muxer, frame, c->rctx.audio_si);
     av_frame_unref(frame);
     break;
   }
 
+  // update sample counters
   c->num_total_samples += nb_samples;
   c->num_frame_samples += nb_samples;
   c->num_samples_from_last_seek += nb_samples;
--- sve2/context/context.h
+++ sve2/context/context.h
@@ -15,77 +15,324 @@
 
 // sve2 context provides a graphics context (OpenGL ES) and an audio context
 // e.g. when in preview mode, a GLFW window will be created, and audio will be
-// played by OpenAL, but in render mode, both video and audio will be "piped" to
-// an output context. An OpenGL context is still provided for
-// hardware-accelerated rendering
+// played by miniaudio, but in render mode, both video and audio will be "piped"
+// to a muxer. An OpenGL context is still provided for hardware-accelerated
+// rendering.
 typedef enum { CONTEXT_MODE_PREVIEW, CONTEXT_MODE_RENDER } context_mode_t;
 
 typedef struct {
+  /**
+   * @brief Context mode, see the docs of context_mode_t for more details.
+   */
   context_mode_t mode;
-  i32 width, height, fps, sample_rate, num_buffered_audio_frames;
+  /**
+   * @brief Self-explanatory
+   */
+  i32 width, height, fps, sample_rate;
+  /**
+   * @brief Number of buffered frames for audio preview. This is ignored in
+   * render mode.
+   */
+  i32 num_buffered_audio_frames;
+  /**
+   * @brief Pointer to the channel layout. Currently supported layout are mono
+   * (AV_CHANNEL_LAYOUT_MONO) and stereo (AV_CHANNEL_LAYOUT_STEREO). Other
+   * layout may or may not work.
+   */
   const AVChannelLayout *ch_layout;
+  /**
+   * @brief Audio sample format. Must be a non-planar format.
+   */
   enum AVSampleFormat sample_fmt;
+  /**
+   * @brief Path to the media file outputted in render mode. Ignored in preview
+   * mode.
+   */
   const char *output_path;
 } context_init_t;
 
+/**
+ * @brief Preview context, only defined in preview mode
+ */
 typedef struct {
+  /**
+   * @brief miniaudio audio device
+   */
   ma_device audio_device;
+  /**
+   * @brief Mutex for audio_fifo
+   */
   mtx_t audio_fifo_mutex;
+  /**
+   * @brief Internal buffer for audio playback
+   */
   AVAudioFifo *audio_fifo;
+  /**
+   * @brief Internal staging buffer returned by context_map_audio()
+   */
   void *audio_staging_buffer;
 } preview_context_t;
 
+/**
+ * @brief Render context, only defined in render mode
+ */
 typedef struct {
+  /**
+   * @brief Muxer for output file
+   */
   muxer_t muxer;
+  /**
+   * @brief GPU-owned video frame submitted to hardware-accelerated video
+   * encoder for video rendering
+   */
   AVFrame *hw_frame;
+  /**
+   * @brief Temporary frame for transferring data to hw_frame (video)/muxer
+   * (audio).
+   */
   AVFrame *transfer_frame;
+  /**
+   * @brief Color conversion shader (from RGB to YUV) for encoding.
+   */
   shader_t *color_convert_shader;
+  /**
+   * @brief Stream indices of output media file.
+   */
   i32 video_si, audio_si;
+  /**
+   * @brief Cached return value from hw_align_size().
+   */
   i32 uv_offset_y;
+  /**
+   * @brief OpenGL objects for capturing rendering output.
+   */
   GLuint fbo, fbo_color_attachment, output_texture;
 } render_context_t;
 
 typedef struct context_t {
+  /**
+   * @brief Context initialization info, copied from the struct specified in
+   * context_init()
+   */
   context_init_t info;
+  /**
+   * @brief GLFW window (wrapping both the native window and the GL context)
+   */
   GLFWwindow *window;
+  /**
+   * @brief Global shader manager, managing all context GL shaders
+   */
   shader_manager_t sman;
-  // video timing
+  /**
+   * @brief Frame number counter, increased by 1 in every call to
+   * context_begin_frame()
+   */
   i32 frame_num;
-  // audio timing
+  /**
+   * @brief Offset of audio timer. This is used to implement seeking for this
+   * timer.
+   */
   i64 audio_timer_offset;
+  /**
+   * @brief Number of samples since last seek of audio timer. This is used to
+   * implement seeking for this timer.
+   */
   i32 num_samples_from_last_seek;
+  /**
+   * @brief Total number of samples played.
+   */
   i32 num_total_samples;
+  /**
+   * @brief Total number of samples played in this frame.
+   */
   i32 num_frame_samples;
+  /**
+   * @brief Content scaling. This is used to support HiDPI displays (not like
+   * it's any useful for this type of application though).
+   */
   f32 xscale, yscale;
+  /**
+   * @brief User pointer. Set/get using
+   * context_set_user_pointer/context_get_user_pointer. This is used to retrieve
+   * contexts in GLFW event callbacks.
+   */
   void *user_ptr;
 
+  /**
+   * @brief Render-mode-specific context
+   */
   render_context_t rctx;
+  /**
+   * @brief Preview-mode-specific context
+   */
   preview_context_t pctx;
 } context_t;
 
+/**
+ * @brief Initialize the context with parameters specified in info struct.
+ * Using two contexts at the same time is undefined behavior. To use another
+ * context, one must free the existing context (using context_free()). This
+ * function can only be called on the main thread.
+ *
+ * @param info Context creation parameters, see the docs for context_init_t
+ * for more details
+ * @return Pointer to the heap-allocated context. The performance implication of
+ * heap-allocation here is negligible since context initialization is not the
+ * primary bottleneck (as it is usually done at most once every invocation).
+ */
 context_t *context_init(const context_init_t *info);
+
+/**
+ * @brief Free an initialized context. This frees up every resources owned by
+ * the context.
+ *
+ * @param c The existing context previously initialized by context_init()
+ */
 void context_free(context_t *c);
 
+/**
+ * @brief Set whether the context should be closed (freed) or not. This does not
+ * free the context (as it must be manually done via context_free).
+ *
+ * @param c The context
+ * @param should_close A boolean indicating whether the context should be closed
+ * or not.
+ */
 void context_set_should_close(context_t *c, bool should_close);
+/**
+ * @brief Get whether the context should be closed (freed) or not. This flag can
+ * be set by an external source (e.g. user closing the preview window) or set by
+ * context_set_should_close.
+ *
+ * This is meant to be used as following:
+ * - First the user initialize a context c.
+ * - Then, loops until context_get_should_close(c) is true, and free the context
+ * after the loop finishes
+ * - In the loop, render media and handle user events. If something happened and
+ * the application is meant to exit after the current loop iteration, use
+ * context_set_should_close to set the flag to true.
+ *
+ * @param c The context
+ * @return A boolean indicating whether the context should be closed or not
+ */
 bool context_get_should_close(context_t *c);
 
-i64 context_get_time(context_t *c);
+/**
+ * @brief Get information related to the main framebuffer for rendering. Pass a
+ * pointer (or NULL if not interested in the information) as the function
+ * arguments to retrieve the necessary parameters.
+ *
+ * @param c The context
+ * @param w Framebuffer width
+ * @param h Framebuffer height
+ * @param xscale Horizontal content scale
+ * @param yscale Vertical content scale
+ */
 void context_get_framebuffer_info(context_t *c, i32 *w, i32 *h, f32 *xscale,
                                   f32 *yscale);
+/**
+ * @brief Begin rendering (video and audio) for a new frame. To end rendering,
+ * call context_end_frame(). Invocations to the two functions must match.
+ *
+ * @param c The context
+ */
 void context_begin_frame(context_t *c);
+/**
+ * @brief End rendering (video and audio) for a frame, started by
+ * context_begin_frame(). Invocations to the two functions must match.
+ *
+ * @param c The context
+ */
 void context_end_frame(context_t *c);
+
+/**
+ * @brief Get the main (default) framebuffer for rendering. This is the 0
+ * framebuffer in preview mode, and an internal framebuffer in rendering mode.
+ * Graphics rendered to this framebuffer will be shown on the preview window
+ * (preview mode) or encoded to the output media (render mode).
+ *
+ * @param c The context
+ * @return The OpenGL handle of the main framebuffer
+ */
 GLuint context_default_framebuffer(context_t *c);
 
+/**
+ * @brief Set the context audio timer to time. This is used to seek the global
+ * timer.
+ *
+ * @param c The context
+ * @param time The new timer value, measured in nanoseconds
+ */
 void context_set_audio_timer(context_t *c, i64 time);
+
+/**
+ * @brief Get the current audio timer. This serves as the global timer, and is
+ * based on the number of samples played. Video is expected to be synchronized
+ * with this timer.
+ *
+ * @param c The context
+ * @return The value of the audio timer in nanoseconds
+ */
 i64 context_get_audio_timer(context_t *c);
 
+/**
+ * @brief Begin transferring audio samples to audio device. At most *nb_samples
+ * samples (per channel) can be written to *staging_buffer.
+ *
+ * @param c The context
+ * @param staging_buffer Pointer to return the address of the audio staging
+ * buffer. This must not be NULL.
+ * @param nb_samples Pointer to return the maximum number of writable samples
+ * (per channel) in *staging_buffer.
+ * @return Whether the map operation succeeded. The operation fails when the
+ * audio buffer is full.
+ */
 bool context_map_audio(context_t *c, void *staging_buffer[static 1],
                        i32 nb_samples[static 1]);
+/**
+ * @brief Submit audio samples to audio device. Samples are written to the
+ * staging buffer returned by context_map_audio. This function could only be
+ * called when the context_map_audio operation succeeded.
+ *
+ * @param c The context
+ * @param nb_samples Number of samples (per channel) written to the staging
+ * buffer. This must be at most the returned number of samples returned from
+ * context_map_audio.
+ */
 void context_unmap_audio(context_t *c, i32 nb_samples);
 
+/**
+ * @brief Set user pointer to context
+ *
+ * @param c The context
+ * @param u The user pointer
+ */
 void context_set_user_pointer(context_t *c, void *u);
+/**
+ * @brief Get user pointer of context owning window
+ *
+ * @param window GLFW window owned by the current context
+ */
 void *context_get_user_pointer(GLFWwindow *window);
+/**
+ * @brief Get the context owning a specific window
+ *
+ * @param window A GLFW window owned by a context
+ * @return The context owning window
+ */
 context_t *context_get_from_window(GLFWwindow *window);
+/**
+ * @brief Get the shader manager of a context
+ *
+ * @param c The context
+ * @return The shader manager of the context c.
+ */
 shader_manager_t *context_get_shader_manager(context_t *c);
 
+/**
+ * @brief Set GLFW key callback to window owned by context c
+ *
+ * @param c The context
+ * @param key New GLFW key callback of the window
+ */
 void context_set_key_callback(context_t *c, GLFWkeyfun key);
